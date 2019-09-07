4d227f51cdf9407f5a266046c215a6eaa53fcac1
Maciej Krüger <mkg20001@gmail.com>
2019-09-08 01:19:51 +0200

    fix: code is not string

--- src/HtmlTransformer.js
+++ src/HtmlTransformer.js
@@ -6,7 +6,7 @@ const logic = require('./logic')
 module.exports = new Transformer({
   async transform ({asset, config, options}) {
     if (!options.hmr) {
-      const {processedHtml} = logic(asset.getCode())
+      const {processedHtml} = logic(await asset.getCode())
 
       asset.setCode(processedHtml)
     }
